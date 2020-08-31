a7d1194ff6d85a07ffa0dabe8c442350912f0bd2
Pete <38692750+PavelRasiukevich@users.noreply.github.com>
2020-08-31 20:23:14 +0300

    movement through waypoints

---
+++ TDGame/TDGame/Assets/Scripts/EnemyScripts/MovementThroughWaypoints.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MovementThroughWaypoints : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+    private int index;
+
+    private void Start()
+    {
+        target = WaypointCollector.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 destination = target.position - transform.position;
+
+        transform.Translate(destination.normalized * speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position,target.position) <= 0.2f)
+        {
+            SetNewDestinationPoint();
+        }
+    }
+
+    private void SetNewDestinationPoint()
+    {
+        if (index >= WaypointCollector.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        index++;
+        target = WaypointCollector.points[index];
+    }
+}
